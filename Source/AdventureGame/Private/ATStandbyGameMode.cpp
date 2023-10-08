d8acb72ef4f112f3a778368379a47c479eb128f0
hyoslog <98869559+hyoslog@users.noreply.github.com>
2023-10-09 00:47:10 +0900

    대기 화면 GameMode 및 GameState 생성
    + 대기 화면 레벨에서 사용할 GameMode 및 GameState 클래스 생성
      - 디폴트 클래스로 지정

---
+++ Source/AdventureGame/Private/ATStandbyGameMode.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ATStandbyGameMode.h"
+#include "ATStandbyGameState.h"
+
+AATStandbyGameMode::AATStandbyGameMode()
+{
+	GameStateClass = AATStandbyGameState::StaticClass();
+}
