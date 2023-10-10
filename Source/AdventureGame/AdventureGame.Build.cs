3c42516460e67d0162b43f2e77d72a1f34552ce5
hyostory <hyoslog@gmail.com>
2023-10-10 09:07:40 +0900

    모듈 추가
    + ModularGameplay

--- Source/AdventureGame/AdventureGame.Build.cs
+++ Source/AdventureGame/AdventureGame.Build.cs
@@ -8,7 +8,7 @@ public class AdventureGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ModularGameplay" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
